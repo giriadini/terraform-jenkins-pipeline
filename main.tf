805c05ed57ed4525f16f2fb3303aad88abf5b0e2
giriadini <giri62535@gmail.com>
2020-08-02 01:24:25 +1000

    modified tf files

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
-  vpc_security_group_ids = [aws_security_group.default.id]
   instance_type          = var.instance_type
   tags = {
     Name = "terraform-default"
